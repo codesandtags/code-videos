8717a49842857b4f63a141cfe784bb41411de358
Edwin Torres <codesandtags@gmail.com>
2022-12-22 13:27:24 -0500

    feat: added array and string methods

---
+++ javascript/questions/js-string-methods.js
@@ -0,0 +1,25 @@
+// 22 JavaScript String Methods
+'Hola Mamá'.charAt(0);
+'Hola Mamá'.concat(' 👋 ');
+'Hola Mamá'.startsWith('H');
+'Hola Mamá'.endsWith('á');
+'Hola Mamá'.includes('W');
+'Hola Mamá'.indexOf('a');
+'Hola Mamá'.lastIndexOf('a');
+'Hola Mamá'.match(/[A-Z]/g);
+'Hola Mamá'.padStart(12, 'X');
+'Hola Mamá'.padEnd(12, 'X');
+'Hola '.repeat(3);
+'Hola Mamá'.replace('a', '4');
+'Hola Mamá'.replaceAll('a', '4');
+'Hola Mamá'.search('la');
+'Hola Mamá'.slice(1,4);
+'Hola Mamá'.substring(1,4);
+'Hola Mamá'.split(' ');
+'Hola Mamá'.toLowerCase();
+'Hola Mamá'.toUpperCase();
+`[${'  Hola Mamá  '.trim()}]`;
+`[${'  Hola Mamá  '.trimStart()}]`;
+`[${'  Hola Mamá  '.trimEnd()}]`;
+// 💻 Codesandtags 🔥
+
