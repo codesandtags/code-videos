80c5b801d7df212b829988c7459a059e01d912fa
Edwin Torres <codesandtags@gmail.com>
2023-01-19 19:51:19 -0500

    feat: added remaining files

--- javascript/questions/synctactic-sugar.js
+++ javascript/questions/synctactic-sugar.js
@@ -1,1 +1,24 @@
-// ¿Qué es syntactic sugar en JavaSCript?
+// ¿Qué es Syntactic Sugar en JavaSCript?
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+Muy bien, "Syntactic sugar" es un término utilizado para referirnos 
+a una sintaxis o expresión, que provee la misma funcionalidad 
+de algo que ya existe sólo que de una forma más corta, más, 
+fácil de escribir y de leer.
+
+Pero veamos algunos ejemplos.
+*/
