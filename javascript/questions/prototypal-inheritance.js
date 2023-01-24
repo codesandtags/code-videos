b4040894bb338f1f977d5148605a3c8884f0e0cb
Edwin Torres <codesandtags@gmail.com>
2023-01-23 23:01:10 -0500

    feat: added examples

--- javascript/questions/prototypal-inheritance.js
+++ javascript/questions/prototypal-inheritance.js
@@ -11,3 +11,48 @@ console.log(person.isHuman);
 
 console.log(child);
 console.log(child.isHuman);
+
+
+function Animal(name) {
+  this.name = name;
+  this.home = 'La tierra 🌎';
+}
+
+let misterio = new Animal('misterio');
+
+Object.getPrototypeOf(misterio);
+
+function LandMammal(legs) {
+  this.legs = legs;
+}
+
+
+class Animal {
+  constructor(name) {
+    this.name = name;
+    this.home = 'La tierra 🌎';
+  }
+}
+
+class LandMammal extends Animal {}
+
+class Dog extends LandMammal {
+  constructor(name, legs) {
+    super(name);
+
+    this.legs = legs;
+  }
+}
+
+let coco = new Dog('coco', 4);
+coco
+
+coco instanceof Dog
+coco instanceof Animal
+coco instanceof Object
+coco instanceof Array 
+
+Animal.prototype.speak = function () {
+  console.log(`Hi I am ${this.name}!`);
+}
+
